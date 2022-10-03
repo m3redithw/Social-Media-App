15c1b9ba827273651bff116520a213c5da85744b
Meredith Wang <wang.meredith09@gmail.com>
2022-10-03 11:41:52 -0500

    WIP

--- Home.py
+++ Home.py
@@ -9,36 +9,24 @@ from PIL import Image
 # functions
 import prepare
 
-
-# read in data
-
-
-df = pd.read_csv('data/tiktok_data.csv')
-
-# clean df
-df = prepare.prep_tiktok(df)
-
-# set date as index
-df.set_index('date', inplace=True)
-df = df.sort_index()
-
-# trim off dates
-df = df[df.index >= '2018-12-01']
-df = df[df.index <= '2022-09-10']
-
-# train dataset
-train_size = int(round(df.shape[0] * 0.5)+1)
-train = df[:train_size]
-train = train.sort_index()
-
-
+# page config
 st.set_page_config(page_title='Home',
                    page_icon='😀',
                    layout="wide")
-image = Image.open('img/title.png')
-st.image(image,use_column_width=True)
-# st.sidebar.success("Select a page above.")
 
-# st.title('TikTok Engagement Dashboard')
-# st.markdown('#### The Rise and Fall of Social Media')
+# tabs
+columns = st.columns((0.5,1,0.5))
+with columns[1]:
 
+    tab1, tab2 = st.tabs(['Following', 'For You'])
+
+    with tab1:
+        st.text('work in progress')
+
+    with tab2:
+        image = Image.open('img/foryou.png')
+        st.image(image,use_column_width=True)
+
+with columns[2]:
+    image = Image.open('img/search.png')
+    st.image(image,width=100)
